f16676f464a958295a2373693419d88998b6e812
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-12-02 11:50:04 -0600

    day 2 part 1

--- Day02/day02.py
+++ Day02/day02.py
@@ -1,12 +1,44 @@
 from pathlib import Path
+from pprint import *
 
 
 def part1(filename):
-    ans = 0
     p = Path(__file__).with_name(filename)
     with p.open("r") as f:
         lines = f.readlines()
-    return 0
+
+    def getColorDict(s):
+        d = dict()
+        s = s.split(", ")
+        for i in s:
+            i = i.split(" ")
+            num = int(i[0])
+            color = i[1]
+            d[color] = num
+        return d
+
+    subt = 0
+    maxNumDict = {"blue": 14, "green": 13, "red": 12}
+    lines = [i.strip() for i in lines]
+    for i in range(len(lines)):
+        lines[i] = lines[i].split(": ")[1:]
+    lines = [i[0].split("; ") for i in lines]
+    for i in range(len(lines)):
+        for j in range(len(lines[i])):
+            lines[i][j] = getColorDict(lines[i][j])
+    lines.insert(0, [dict()])
+    for idx, game in enumerate(lines):
+        if idx == 0:
+            continue
+        for roll in game:
+            if (
+                roll.get("blue", -1) > maxNumDict["blue"]
+                or roll.get("red", -1) > maxNumDict["red"]
+                or roll.get("green", -1) > maxNumDict["green"]
+            ):
+                subt += idx
+                break
+    return 50 * 101 - subt
 
 
 def part2(filename):
