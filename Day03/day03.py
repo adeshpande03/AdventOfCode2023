ff0e51c9a40888aba8988f1eaf307bc8a9e5714f
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-12-03 13:56:30 -0600

    cleanup

--- Day03/day03.py
+++ Day03/day03.py
@@ -39,8 +39,7 @@ def part2(filename):
         lines = f.read().splitlines()
     ans = 0
     lines = [list(i) for i in lines]
-    num = ""
-    replacearr = []
+    num, replacearr = "", []
     for i in range(len(lines)):
         for j in range(len(lines[i])):
             if lines[i][j].isdigit():
@@ -55,14 +54,14 @@ def part2(filename):
     temp = set()
     for i in range(1, len(lines) - 1):
         for j in range(1, len(lines[i]) - 1):
-            if not lines[i][j].isalnum() and not lines[i][j] == ".":
+            if lines[i][j] == "*":
                 for k in range(i - 1, i + 2):
                     for l in range(j - 1, j + 2):
                         if lines[k][l].isdigit():
                             temp.add(lines[k][l])
                 if len(temp) == 2:
                     temp = list(temp)
-                    ans += (int(temp[0]) * int(temp[1]))
+                    ans += int(temp[0]) * int(temp[1])
                 temp = set()
     return ans
 
