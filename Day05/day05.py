464bf4591a4c32fcc64b129f5a5ed8e62723f3ec
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-12-04 12:44:31 -0600

    day 5 setup

---
+++ Day05/day05.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from pprint import *
+
+
+def part1(filename):
+    with Path(__file__).with_name(filename).open("r") as f:
+        lines = f.read().splitlines()
+
+def part2(filename):
+    with Path(__file__).with_name(filename).open("r") as f:
+        lines = f.read().splitlines()
+        
+
+if __name__ == "__main__":
+    print(part1("input.txt"))
+    print(part2("input.txt"))
