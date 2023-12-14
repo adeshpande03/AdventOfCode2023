005aa0884a11b6aa4c09c95f14a394056e2caf48
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-12-14 10:45:08 -0600

    tuple tuple tuple

--- Day14/day14.py
+++ Day14/day14.py
@@ -36,31 +36,37 @@ def part1(filename):
 def part2(filename):
     with Path(__file__).with_name(filename).open("r") as f:
         lines = f.read().splitlines()
+    @cache
+    def cycle(lines):
+        for _ in range(4):
+            lines = list(zip(*reversed(lines)))
+            lines = ["".join(i).split("#") for i in lines]
+            for i in range(len(lines)):
+                for j in range(len(lines[i])):
+                    lines[i][j] = sorted(lines[i][j])
 
-    def cycle(lines, n):
-        for __ in range(n):
-            for _ in range(4):
-                lines = list(zip(*reversed(lines)))
-                lines = ["".join(i).split("#") for i in lines]
-                for i in range(len(lines)):
-                    for j in range(len(lines[i])):
-                        lines[i][j] = sorted(lines[i][j])
-
-                new = []
-                for i in lines:
-                    temp = []
-                    for j in i:
-                        if not j:
-                            temp.append("#")
-                            continue
-                        else:
-                            temp += j
-                            temp += ["#"]
-                    new.append(temp[:-1])
-                lines = new
-        return lines
-
-    lines = cycle(lines, 1000)
+            new = []
+            for i in lines:
+                temp = []
+                for j in i:
+                    if not j:
+                        temp.append("#")
+                        continue
+                    else:
+                        temp += j
+                        temp += ["#"]
+                new.append(tuple(temp[:-1]))
+            lines = new
+        return tuple(lines)
+    d = dict()
+    for _ in range(1000):
+        lines = tuple(lines)
+        if tuple(lines) in d:
+            lines = tuple(d[tuple(lines)])
+        else:
+            d[lines] = tuple(cycle(lines))
+            lines = tuple(cycle(lines))
+            
     lines = list(zip(*reversed(lines)))
     ans = 0
     for i in range(len(lines)):
