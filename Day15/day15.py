4fecef3545f9dc2f50100cbe9aae19f8b79a7793
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-12-15 01:18:50 -0600

    spacing

--- Day15/day15.py
+++ Day15/day15.py
@@ -8,29 +8,34 @@ from collections import OrderedDict
 def part1(filename):
     with Path(__file__).with_name(filename).open("r") as f:
         lines = f.read()
-    lines = lines.split(',')
+    lines = lines.split(",")
+
     def sha(s):
         v = 0
         for i in s:
             v += ord(i)
-            v =  (v * 17) % 256
+            v = (v * 17) % 256
         return v
+
     return sum(list(map(sha, [i.strip() for i in lines])))
+
 
 def part2(filename):
     with Path(__file__).with_name(filename).open("r") as f:
         lines = f.read()
-    lines = lines.split(',')
+    lines = lines.split(",")
     lines = [i.strip() for i in lines]
     ans = [OrderedDict() for _ in range(256)]
+
     def sha(s):
         v = 0
         for i in s:
             v += ord(i)
-            v =  (v * 17) % 256
+            v = (v * 17) % 256
         return v
+
     for i in lines:
-        i = i.split('=')
+        i = i.split("=")
         if len(i) == 1:
             key = i[0][:-1]
             enc = sha(key)
@@ -42,12 +47,13 @@ def part2(filename):
             focal = i[1]
             enc = sha(key)
             ans[enc][key] = focal
-    
+
     total = 0
     for i, j in enumerate(ans):
         for c, d in enumerate(j):
-            total += ((i + 1) * (c + 1) *  int(j[d]))
+            total += (i + 1) * (c + 1) * int(j[d])
     return total
+
 
 if __name__ == "__main__":
     print(part1("input.txt"))
