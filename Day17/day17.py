9d4b0bdaf911abd105272bb424ff89e52b5eb16c
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-12-16 23:41:35 -0600

    fixes to plagiarism

--- Day17/day17.py
+++ Day17/day17.py
@@ -5,86 +5,54 @@ import sys
 from heapq import heappop, heappush
 
 
+DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+
+def inr(pos, arr):
+    return pos[0] in range(len(arr)) and pos[1] in range(len(arr[0]))
+
+
+def run(mindist, maxdist, ll):
+    q = [(0, 0, 0, -1)]
+    seen = set()
+    costs = {}
+    while q:
+        cost, x, y, dd = heappop(q)
+        if x == len(ll) - 1 and y == len(ll[0]) - 1:
+            return cost
+        if (x, y, dd) in seen:
+            continue
+        seen.add((x, y, dd))
+        for direction in range(4):
+            costincrease = 0
+            if direction == dd or (direction + 2) % 4 == dd:
+                continue
+            for distance in range(1, maxdist + 1):
+                xx = x + DIRS[direction][0] * distance
+                yy = y + DIRS[direction][1] * distance
+                if inr((xx, yy), ll):
+                    costincrease += ll[xx][yy]
+                    if distance < mindist:
+                        continue
+                    nc = cost + costincrease
+                    if costs.get((xx, yy, direction), 1e100) <= nc:
+                        continue
+                    costs[(xx, yy, direction)] = nc
+                    heappush(q, (nc, xx, yy, direction))
+
+
 def part1(filename):
     with Path(__file__).with_name(filename).open("r") as f:
         lines = f.read().splitlines()
     lines = [list(map(int, list(i))) for i in lines]
-    ll = lines
-    DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-    def inr(pos, arr):
-        return pos[0] in range(len(arr)) and pos[1] in range(len(arr[0]))
-
-    def run(mindist, maxdist):
-        q = [(0, 0, 0, -1)]
-        seen = set()
-        costs = {}
-        while q:
-            cost, x, y, dd = heappop(q)
-            if x == len(ll) - 1 and y == len(ll[0]) - 1:
-                return cost
-            if (x, y, dd) in seen:
-                continue
-            seen.add((x, y, dd))
-            for direction in range(4):
-                costincrease = 0
-                if direction == dd or (direction + 2) % 4 == dd:
-                    continue
-                for distance in range(1, maxdist + 1):
-                    xx = x + DIRS[direction][0] * distance
-                    yy = y + DIRS[direction][1] * distance
-                    if inr((xx, yy), ll):
-                        costincrease += ll[xx][yy]
-                        if distance < mindist:
-                            continue
-                        nc = cost + costincrease
-                        if costs.get((xx, yy, direction), 1e100) <= nc:
-                            continue
-                        costs[(xx, yy, direction)] = nc
-                        heappush(q, (nc, xx, yy, direction))
-
-    return run(1, 3)
+    return run(1, 3, lines)
 
 
 def part2(filename):
     with Path(__file__).with_name(filename).open("r") as f:
         lines = f.read().splitlines()
     lines = [list(map(int, list(i))) for i in lines]
-    ll = lines
-    DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-    def inr(pos, arr):
-        return pos[0] in range(len(arr)) and pos[1] in range(len(arr[0]))
-
-    def run(mindist, maxdist):
-        q = [(0, 0, 0, -1)]
-        seen = set()
-        costs = {}
-        while q:
-            cost, x, y, dd = heappop(q)
-            if x == len(ll) - 1 and y == len(ll[0]) - 1:
-                return cost
-            if (x, y, dd) in seen:
-                continue
-            seen.add((x, y, dd))
-            for direction in range(4):
-                costincrease = 0
-                if direction == dd or (direction + 2) % 4 == dd:
-                    continue
-                for distance in range(1, maxdist + 1):
-                    xx = x + DIRS[direction][0] * distance
-                    yy = y + DIRS[direction][1] * distance
-                    if inr((xx, yy), ll):
-                        costincrease += ll[xx][yy]
-                        if distance < mindist:
-                            continue
-                        nc = cost + costincrease
-                        if costs.get((xx, yy, direction), 1e100) <= nc:
-                            continue
-                        costs[(xx, yy, direction)] = nc
-                        heappush(q, (nc, xx, yy, direction))
-
-    return run(4, 10)
+    return run(4, 10, lines)
 
 
 if __name__ == "__main__":
